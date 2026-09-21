3cf7cd4e485dd464c4b6214b4d560a3cef98c5bb
martial0018 <kelvinmartial02@gmail.com>
2026-09-21 21:50:53 +0300

    Complete PWA install setup

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'tnm-mpamba-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/signup.html',
+  '/payment.html',
+  '/transfer.html',
+  '/transfer-pin.html',
+  '/manifest.webmanifest',
+  '/assets/images/mpamba_logo_transparent.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match('/index.html'));
+    })
+  );
+});
